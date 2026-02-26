736da4424828a086a14968fccd58928ec1f5fbc2
Mostafa Saad <mimotigr@gmail.com>
2026-02-26 17:26:40 +0200

    first commit

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,4 +1,10 @@
-import streamlit as st
+import os
 
-st.title("Smart Asset SaaS")
-st.success("التطبيق يعمل بنجاح 🚀")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+stripe.api_key = os.getenv("STRIPE_API_KEY")
+STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY")
